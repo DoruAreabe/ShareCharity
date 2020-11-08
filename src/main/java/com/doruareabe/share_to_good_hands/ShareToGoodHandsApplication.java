b3dd2687c7c4f2b495f73a623601bb8b7b4e24ba
Mykyta Ponomarenko <mykpon3769@gmail.com>
2020-11-08 17:26:12 +0100

    Initial commit

---
+++ src/main/java/com/doruareabe/share_to_good_hands/ShareToGoodHandsApplication.java
@@ -0,0 +1,13 @@
+package com.doruareabe.share_to_good_hands;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareToGoodHandsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShareToGoodHandsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/doruareabe/share_to_good_hands/ShareToGoodHandsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doruareabe.share_to_good_hands;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareToGoodHandsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
